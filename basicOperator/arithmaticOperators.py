f9c5fceb2b18d44e82b39f47dd7f20148ec3b6dd
MazKionkTea <47136221+MazKionkTea@users.noreply.github.com>
2021-07-23 00:02:18 +0700

    Create arithmaticOperators.py
    python arithmatic operator basic

---
+++ basicOperator/arithmaticOperators.py
@@ -0,0 +1,127 @@
+# •	Arithmetic Operators/Operator Aritmatika
+
+def aritmatika():
+    print(
+        """
+        #=====================================================================================#
+        #                                                                                     #
+        #                                                                                     #
+        #                   " Arithmetic Operators/Operator Aritmatika "                      #
+        #                                                                                     #
+        #                                                                                     #
+        #           Bahasa Python mendukung tipe operator berikut                             #
+        #                                                                                     #
+        #       Asumsikan variabel a memiliki nilai 10 dan variabel b memiliki nilai 21       #
+        #                                                                                     #
+        #        [1]  +   (Penjumlahan)          Contoh : a + b = 31                          #
+        #        [2]  -   (Pengurangan)          Contoh : a – b = -11                         #
+        #        [3]  *   (Perkalian)            Contoh : a * b = 210                         #
+        #        [4]  /   (Pembagian)            Contoh : b / a = 2.1                         #
+        #        [5]  **  (Perpangkatan)         Contoh : a**b =10 to the power 20            #
+        #        [6]  %   (Modulus)              Contoh : b % a = 1                           #
+        #        [7]  //  (Floor Division)       Contoh : 9//2 = 4 and 9.0//2.0 = 4.0         #
+        #                                                                                     #
+        #                                                                                     #
+        #    [00]   Back                                                                      #
+        #    [99]   Exit                                                                      #
+        #                                                                                     #
+        #                                                                                     #
+        #=====================================================================================#
+        """
+    )
+
+# Addition/penjumlahan
+def tambah(a, b):
+    c = a + b
+    print(
+    "=" * 20, "Penjumlahan", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "+", b, "=", c)
+    print("\n", "=" * 50)
+    return c
+# tambah(10, 3)
+
+# Subtraction/pengurangan
+def kurang(a, b):
+    c = a - b
+    print(
+    "=" * 20, "Pengurangan", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "-", b, "=", c)
+    print("\n", "=" * 50)
+    return c
+# kurang(15, 2)
+
+# Multiplication/perkalian
+def kali(a, b):
+    c = a * b
+    print(
+    "=" * 20, "Perkalian", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "x", b, "=", c)
+    print("\n", "=" * 50)
+    return c
+# kali(5, 5)
+
+# Division/pembagian
+def bagi(a, b):
+    c = a / b
+    print(
+    "=" * 20, "Pembagian", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "/", b, "=", c)
+    print("\n", "=" * 50)
+    return c
+# bagi(10, 3)
+
+# Exponent/Pangkat Kuadrat
+def kuadrat(a, b):
+    c = a ** b
+    print(
+    "=" * 20, "Pangkat Kuadrat", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "**", b, "=", c)
+    print("\n", "=" * 50)
+    return c
+# kuadrat(2, 5)
+
+# Modulus/SisaBagi Hasil
+# jika sisa pembagian 1 = ganjil
+# jika sisa pembagian 0 = genap
+def modulus(a, b):
+    c = a % b
+    print(
+    "=" * 20, "Modulus", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "%", b, "=", c)
+    print("\n", "=" * 50)
+    return c 
+# modulus(13, 3)
+
+# floorDivision
+# Pembagian operan di mana hasilnya adalah quotient
+# # di mana digit setelah koma desimal dihapus
+def division(a, b):
+    # 9//2 = 4 and 9.0//2.0 = 4.0, -11//3 = -4, -11.0//3 = -4.0
+    c = a // b
+    print(
+    "=" * 20, "Floor Division", "=" * 20,
+    """
+    """
+    )
+    print(">>>  ", a, "//", b, "=", c)
+    print("\n", "=" * 50)
+    return c
+# division(16, 3)
