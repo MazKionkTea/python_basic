f5e5404aa458ac6da461f4157ae90fdcc4f5c300
Kionk <47136221+MazKionkTea@users.noreply.github.com>
2021-08-23 13:04:29 +0000

    Add files via upload

---
+++ exceptionHandling/exceptionHandling.py
@@ -0,0 +1,7 @@
+"""
+python menyerdiakan dua fitur penting untuk menghandle 
+apapun error yang tidak diinginkan dalam program python 
+kita dan untuk menambahkan kemampuan debugging.
+
+Debug...
+"""
