3869848b2acfcbf21aed6bdd54eaa880cc39ca7f
Kionk <47136221+MazKionkTea@users.noreply.github.com>
2022-04-17 02:53:58 +0700

    csv

---
+++ csv/class.py
@@ -0,0 +1,19 @@
+# class
+class Hero():
+    '''class definition with nameclass Hero'''
+
+    # variable
+    heroes = 0
+    '''variable class'''
+
+    # instantiation
+    def __init__(self, name, health, power, armor):
+        '''instanciating atribute of all heroes'''
+        self.name = name
+        self.health = health
+        self.power = power
+        self.armor = armor
+    
+    heroes =+ 1
+
+    
